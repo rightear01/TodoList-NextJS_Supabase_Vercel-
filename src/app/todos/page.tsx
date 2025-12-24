903b85c1fc7de32956c4165c7009b9484200cfb8
Cha jeong hawn <rightear01@gmail.com>
2025-12-24 16:14:36 +0900

    refactor: add responsive styling to todo components

--- src/app/todos/page.tsx
+++ src/app/todos/page.tsx
@@ -30,12 +30,12 @@ export default async function TodosPage() {
   return (
     <div className="max-w-2xl mx-auto mt-8 p-4">
       <div className="flex flex-col items-center">
-        <h1 className="text-4xl font-bold mb-4 text-shadow-lg text-white">Personal Todo List</h1>
-        <div className="w-[80%] h-[1px] bg-gray-200 mb-20" />
-        <div className="flex gap-4 items-center">
+        <h1 className="max-sm:text-3xl sm:text-5xl font-bold sm:mb-4 text-shadow-lg text-white">Personal Todo List</h1>
+        <div className="sm:w-[80%] h-px bg-gray-200 sm:mb-20 max-sm:mb-10" />
+        <div className="flex gap-4 items-center max-sm:w-full max-sm:justify-center">
           <SignedOut>
             <SignInButton mode="modal">
-              <button className="w-sm min-h-[80px] text-3xl px-4 py-2 bg-blue-500 font-bold text-white rounded hover:bg-blue-600 cursor-pointer shadow-lg text-shadow-lg transition">
+              <button className="max-sm:w-[80%] max-sm:text-2xl sm:w-sm sm:text-3xl px-4 py-2 bg-blue-500 font-bold text-white rounded hover:bg-blue-600 cursor-pointer shadow-lg text-shadow-lg transition">
                 Login
               </button>
             </SignInButton>
@@ -44,7 +44,7 @@ export default async function TodosPage() {
       </div>
       <div className="flex flex-col items-center mb-8 w-full">
         <SignedIn>
-          <h1 className="text-2xl font-bold mb-6 w-full flex justify-between items-center  text-shadow-lg text-gray-700">
+          <h1 className="max-sm:text-xl sm:text-2xl font-bold mb-6 w-full flex justify-between items-center text-shadow-lg text-gray-700">
             Todo List [Total : {todos.length}]{' '}
             <div className="transform scale-130 pr-6">
               <UserButton />
